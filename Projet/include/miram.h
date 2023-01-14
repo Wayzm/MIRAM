4cbf3c0015254ead8dee5f7c1e8dc6622c4f9140
Wayzm <wyeumo@yahoo.fr>
2023-01-14 16:45:47 +0100

    Added description for arnoldi functions

--- Projet/include/miram.h
+++ Projet/include/miram.h
@@ -2,7 +2,10 @@
 #define MIRAM_H
 
 #include "lib.h"
-
+/***
+ * ArnoldiProjection_Modified computes the reduced model with GEVV and GEMV_Modified in the same loop
+ * ArnoldiProjection_Classic computes the reduced model with GEVV and GEMV_Modified in different loops
+*/
 void ArnoldiProjection_Modified(const unsigned int rows_A,
 								const unsigned int cols_A,
 								const unsigned int n_krylov,
@@ -11,7 +14,7 @@ void ArnoldiProjection_Modified(const unsigned int rows_A,
 								double* __restrict__ matrix_Q,
 								double* __restrict__ matrix_H);
 
-void ArnoldiProjection_Classique(const unsigned int rows_A,
+void ArnoldiProjection_Classic(const unsigned int rows_A,
 								 const unsigned int cols_A,
 								 const unsigned int n_krylov,
 								 const double* vecteur,
